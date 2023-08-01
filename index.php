26d45f00cfe4318d9bd9b9d745b7e6fe9098e3fb
uemurag <uemuragisou@gmail.com>
2023-08-01 15:28:03 +0900

    トップページ修正

--- index.php
+++ index.php
@@ -4,80 +4,84 @@
     <meta charset="UTF-8">
     <title>Ajax動作サンプル</title>
     <script type="text/javascript" src="ajax.js"></script>
-</head>
-<body>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://ajaxzip3.github.io/ajaxzip3.js"></script>
-     <p>690円</p>
-    <form action="process.php" method="POST" id="form">
+</head>
+<body>
+    <p>690円</p>
+    <form action="process.php" method="POST" id="form" onsubmit="return validateForm()">
         <div class="order">
             <div class="ordernumber">
-              <div>
-              <input type="number" min="0" name="itemA" class="" placeholder="Aの注文個数">個
-              </div>
-              <div>
-              <input type="number" min="0" name="itemB" class="" placeholder="Bの注文個数">個
-              </div>   
-              <div>
-              <input type="number" min="0" name="itemC" class="" placeholder="Cの注文個数">個
-              </div>   
+                <div>
+                    <input type="number" min="0" name="itemA" class="" placeholder="Aの注文個数">個
+                </div>
+                <div>
+                    <input type="number" min="0" name="itemB" class="" placeholder="Bの注文個数">個
+                </div>   
+                <div>
+                    <input type="number" min="0" name="itemC" class="" placeholder="Cの注文個数">個
+                </div>   
             </div>
         </div>
         <section class="contact-form">
-        <h2>お客様情報</h2>
+            <h2>お客様情報</h2>
             
-        <div>
-        <label for="name">お名前</label><br>
-            <input type="text" id="name" name="name" autofocus required>
+            <div>
+                <label for="name">お名前</label><br>
+                <input type="text" id="name" name="name" autofocus required>
             </div>
             <div>
-            <label for="email">メールアドレス</label><br>
-            <input type="email" id="email" name="email" placeholder="fullofdreams@inbothhands.com" required>
+                <label for="email">メールアドレス</label><br>
+                <input type="email" id="email" name="email" placeholder="fullofdreams@inbothhands.com" required>
             </div>
             <div>
-            <label>郵便番号(ハイフンもOK)</label><br>
-            <input type="text" name="zip01" maxlength="8" placeholder="1234567" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');">
+                <label>郵便番号(ハイフンもOK)</label><br>
+                <input type="text" name="zip01" maxlength="8" placeholder="1234567" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11');">
             </div>
             <div> 
-            <label>都道府県と以降の住所</label><br>
-            <input type="text" name="addr11" placeholder="大阪府大阪市・・・" style="width:260px">
+                <label>都道府県と以降の住所</label><br>
+                <input type="text" name="addr11" placeholder="大阪府大阪市・・・" style="width:260px">
             </div> 
             <div>
-            <input type="submit" id="submit" value="送信" onclick="showSubmitAlert()">
-
+                <input type="submit" id="submit" value="送信">
             </div>
-        </div>
+        </section>
     </form> 
-    <h1>在庫一覧</h1>
-
-    <!-- 在庫情報を表示するためのテーブル -->
-    <table border="1">
-        <tr>
-            <th>商品名</th>
-            <th>在庫数</th>
-        </tr>
-        <?php
-        // get_inventory.phpから在庫情報を取得する
-        $inventoryData = json_decode(@file_get_contents("http://localhost/A-team/get_inventory.php"), true);
-
-        // 在庫情報をテーブルに表示する
-        if ($inventoryData !== null) {
-            $items = array("itemA", "itemB", "itemC");
-            foreach ($items as $item) {
-                echo "<tr>";
-                echo "<td>{$item}</td>";
-                echo "<td>{$inventoryData[$item]}</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='2'>在庫情報が取得できませんでした。</td></tr>";
-        }
-        ?>
-    </table>
-              
     
+    <script>
+        function validateForm() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var zip01 = document.getElementsByName('zip01')[0].value;
+            var addr11 = document.getElementsByName('addr11')[0].value;
+
+            if (name.trim() === '') {
+                alert('お名前を入力してください。');
+                return false;
+            }
+
+            if (!isValidEmail(email)) {
+                alert('有効なメールアドレスを入力してください。');
+                return false;
+            }
+
+            if (zip01.replace('-', '').length !== 7) {
+                alert('有効な郵便番号を入力してください。');
+                return false;
+            }
+
+            if (addr11.trim() === '') {
+                alert('都道府県と住所を入力してください。');
+                return false;
+            }
+
+            return true;
+        }
+
+        function isValidEmail(email) {
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        }
+    </script>
 </body>
 </html>
-
-<!-- 【トップページ】◎表示する -->
-<!-- http://localhost/A-team/index.php -->
